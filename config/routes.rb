ee8cdc9e9c290daa3ed6ec814a45740a84914fb3
rsabha <rsabha@tuta.io>
2020-07-26 14:30:57 +0200

    git push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pricing', to: 'pricing#index'
+  get 'contact', to: 'contact#index'
+  get 'faq', to: 'faq#index'
   get 'welcome/index'
   get 'users', to: 'users#index'
+  get '/user' => "users#index", :as => :user_root
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

0affe9444b5e7dd0c3c9e36aec639986602e55e3
Rudolph Venter <rudolphthefox@gmail.com>
2020-07-31 15:53:50 +0200

    Revealed the secrets of the universe

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,12 @@
+class AdminsController < ApplicationController
+    before_action :authenticate_admin!
+  
+    def index
+        @admin = current_admin
+    end
+
+    def show 
+        @users = User.all
+      end
+  end
+  

5c3a1c913e416f8b428c64c26ea88d53560a1391
Rudolph Venter <rudolphthefox@gmail.com>
2020-08-01 11:11:50 +0200

    Added HTTP auth to admin sign-up

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,12 +1,11 @@
 class AdminsController < ApplicationController
     before_action :authenticate_admin!
-  
+
     def index
         @admin = current_admin
     end
 
     def show 
         @users = User.all
-      end
+    end
   end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :admins, :controllers => {:registrations => 'admins/registrations'}
+  devise_scope :admin do
+    get '/admin' => "admins#index", :as => :admin_root
+    get 'admins', to: 'admins#index'
+    get 'admins/show', to: 'admins#show'
+  end
+
   devise_for :users
+  devise_scope :user do
+    get '/user' => "users#index", :as => :user_root
+    get 'users', to: 'users#index'
+  end
+
+  get 'welcome/index'
+  root 'welcome#index'
+
   get 'pricing', to: 'pricing#index'
   get 'contact', to: 'contact#index'
   get 'faq', to: 'faq#index'
-  get 'welcome/index'
-  get 'users', to: 'users#index'
-  get '/user' => "users#index", :as => :user_root
-
   get '/privacy.txt', :to => redirect('/assets/privacy.txt')
 
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

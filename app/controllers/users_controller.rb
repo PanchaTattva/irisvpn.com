c8af552da258f4ae883df1fbfd355626ea7ce31a
rsabha <rsabha@tuta.io>
2020-08-07 23:56:52 +0200

    ovpn filename fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def download_ovpn_file
     path = "/easy-rsa/vpn_users/"
     user = current_user.email.split('@')[0]
-    @filename ="#{path}#{user}"
+    @filename ="#{path}#{user}.ovpn"
 
     send_file(@filename, :filename => "#{user}.ovpn")
   end

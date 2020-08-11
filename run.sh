3be8972560c262e069d2ee4d8fcb6e2e99e5ffee
Rudolph Venter <rudolphthefox@gmail.com>
2020-08-11 16:14:37 +0200

    Removed scripts that were merged into 'script.sh'

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f tmp/pids/server.pid
-bundle install
-yarn install --check-files;
-RAILS_ENV=production rails db:migrate
-RAILS_ENV=production rails assets:precompile
-rails server -e production

00d6c87645556a2c6aaacdb11b445ed5a877c9fb
rsabha <rsabha@tuta.io>
2020-08-14 15:35:21 +0200

    added webhook rb script for github pushes. it will trigger app builds

---
+++ webhooks/README.md
@@ -0,0 +1,7 @@
+# Webhooks
+
+This script will listen for POST requests to the url: `/github/push-event`.
+
+The Github repo has been configured with a webhook to POST to the above url
+when a push event happens. This script then needs to trigger a new Docker image
+build for the app.
---
+++ webhooks/webhook.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+require 'sinatra'
+require 'json'
+
+post '/github/push-event' do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
+  puts "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), 
+						ENV['SECRET_TOKEN'], 
+						payload_body)
+
+  return halt 500, "Signatures didn't match!" unless 
+	Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
